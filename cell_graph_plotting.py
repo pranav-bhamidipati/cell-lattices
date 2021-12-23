51a41c904f8565369105cf5d1ab0ab7c18b1ed05
pranav-bhamidipati <pbhamidi@usc.edu>
2021-12-23 10:37:53 -0800

    produced UMAP; coloring needs fix

--- cell_graph_plotting.py
+++ cell_graph_plotting.py
@@ -35,21 +35,35 @@ pal = [
 ]
 #color_key = {str(d): c for d, c in enumerate(pal)}
 color_key = pal
+#color_key = plt.get_cmap("Set1").colors
 
 print(f"Reading data from {data_fname}")
 df = pd.read_csv(data_fname)
+df["n_components"] = np.random.randint(1, 5, size=df.shape[0])
 df["n_components"] = df["n_components"].astype("category")
+
 
 print("Plotting with Datashader")
 cvs = ds.Canvas(plot_width=400, plot_height=400)
 agg = cvs.points(df, "umap_x", "umap_y", ds.count_cat("n_components"))
-img = tf.shade(agg, color_key=color_key, how="eq_hist")
+img = tf.shade(agg, color_key=color_key, how="linear")
 
 if save:
-    fname = "cellgraph_embedding"
-    fpath = os.path.join(save_dir, fname + "." + fmt)
-    utils.export_image(img, filename=fpath, background="black")
     
-    print(f"Saving image to {fpath}")
+    # Save UMAP as image
+    imname = "cellgraph_embedding_image"
+    impath = os.path.join(save_dir, imname)
+    utils.export_image(img, filename=impath, background="white")
+    
+    # Make plot from image
+    fname = "cellgraph_embedding_plot"
+    fpath = os.path.join(save_dir, fname + "." + fmt)
+    image = plt.imread(impath + ".png")
+    fig, ax = plt.subplots(figsize=(6, 6))
+    plt.imshow(image)
+    plt.setp(ax, xticks=[], yticks=[])
+    plt.title("UMAP of tissue topologies ($5\mathrm{x}5$)", fontsize=12)
 
+    print(f"Saving figure to {fpath}")
+    plt.savefig(fpath, format=fmt, dpi=dpi)
 
