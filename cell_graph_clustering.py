86d3f769e4c2034b678af00669df3fcfc476a489
pranav-bhamidipati <pbhamidi@usc.edu>
2021-12-22 17:04:07 -0800

    calculate cell graph #components and UMAP

--- cell_graph_clustering.py
+++ cell_graph_clustering.py
@@ -21,9 +21,9 @@ import seaborn as sns
 
 # Options for saving output(s)
 save     = True
-save_dir = os.path.realpath("/home/pbhamidi/git/cell-lattices/plots")
-fmt      = "png"
-dpi      = 300
+save_dir = os.path.realpath("./plots")
+# fmt      = "png"
+# dpi      = 300
 
 # Get coordinates and adjacency for a periodic square lattice
 rows = cols = 5
@@ -40,46 +40,50 @@ n_sub = n // 2
 # Get total number of permutations this tissue can undergo
 ncomb = int(sp.comb(n, n_sub))
 
-# Make generator of all cell type combinations
-gen = combinations(np.arange(n), n_sub)
+# Get all cell type combinations as indices (for computing # components)
+combs_idx = [i for i in combinations(np.arange(n), n_sub)]
 
-# Make Boolean array of all combinations
-combs = np.zeros((ncomb, n), dtype=bool)
-for i in tqdm(range(ncomb)):
-    idx = next(gen)
-    combs[i, idx] = True
+# Get combinations as Boolean data (for UMAP)
+combs_bool = np.zeros((ncomb, n), dtype=bool)
+for i, idx in enumerate(tqdm(combs_idx)):
+    combs_bool[i, idx] = True
 
-def n_connected_components(idx):
-    """Compute number of connected components given cell indices"""
-    return nx.number_connected_components(G.subgraph(idx))
+# Get combinations as strings
+combs_str = ["".join([str(int(c)) for c in _comb]) for _comb in combs_bool]
 
 # Parallelize calculation of tissue topology (# coneccted components)
 if __name__ == '__main__':
     
-    # Get cluster
+    # Get worker pool
     n_workers = mp.cpu_count()
     pool = mp.Pool(n_workers)
     
-    # Compute results as array
-    iterator = combinations(np.arange(n), n_sub)
-    result_list = pool.map(n_connected_components, iterator)
+    # Define computation
+    def n_connected_components(idx):
+        """Computes number of connected components given cell indices"""
+        return nx.number_connected_components(G.subgraph(idx))
+
+    # Perform parallel computation
+    result_list = pool.map(n_connected_components, combs_idx)
     ncc = np.asarray(result_list)
 
-## Perform UMAP
-# Select data
-data_slice = slice(None, None, None)
-# data_slice = slice(0, 3000, 300)
-data       = combs[data_slice]
-clusters   = ncc[data_slice]
-colors     = [sns.color_palette()[i] for i in clusters]
+    ## Perform UMAP
+    # Select data
+    data_slice = slice(None, None, None)
+    # data_slice = slice(0, 3000, 300)
+    data       = combs[data_slice]
+    clusters   = ncc[data_slice]
+    colors     = [sns.color_palette()[i] for i in clusters]
 
-# # Perform UMAP with progress
-# reducer   = umap.UMAP(metric="manhattan", verbose=True)
-# embedding = reducer.fit_transform(combs)
-#####
-embedding = np.random.random((ncombs, 2))
+    # # Perform UMAP with progress
+    # reducer   = umap.UMAP(metric="hamming", verbose=True)
+    # embedding = reducer.fit_transform(combs)
+    
+    #### DUMMY EMBEDDING ############
+    embedding = np.random.random((ncombs, 2))
+    #################################
 
-combs_str = ["".join([str(int(c)) for c in comb]) for comb in combs]
+# Combine into dataframe
 df = pd.DataFrame(dict(
     combination = combs_str,
     n_components = ncc,
@@ -87,9 +91,10 @@ df = pd.DataFrame(dict(
     umap_y = embedding[:, 1],
 ))
 
-data_fname = os.path.join(save_dir, "cellgraph_embedding.csv")
-df.to_csv(data_fname, index=False)
-
+data_fname = "cellgraph_embedding.csv"
+data_fpath = os.path.join(save_dir, data_fname)
+df.to_csv(data_fpath, index=False)
+print(f"Saved to {data_fpath}")
 
 # # Plot
 # plt.scatter(
